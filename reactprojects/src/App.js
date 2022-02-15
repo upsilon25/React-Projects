90049021e3a5d551af7c7e4ce16a37b4a5a3cd93
vijay <Javascript_user>
2022-02-15 19:05:36 +0530

    Intro.js is commited

--- reactprojects/src/App.js
+++ reactprojects/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
+import Intro from './Component/Intro/intro' 
+import './';
 
 
 function App() {
   return (
-    <div  className="App">
-     <h1>This is very important to US</h1>
-      <button className="btn btn-success">thisisiclassnpm st</button> 
-    </div>
+    <div>
+      
+      <Intro />
+    </div> 
    
-  );
+  )
 }
 
 export default App;
---
+++ reactprojects/src/Component/Intro/intro.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./intro.css"
+import me from './img/vp.png'
+const Intro = () =>{
+    return(
+        <div>
+            <div className="i">
+                <div className="i-left">
+                    <div className="i-left-wrapper">
+                        <div className="i-intro"><h2>Hello,My name is </h2></div>
+                        <div className="i-name"> <h1>Vijay Ratan Singh</h1> </div>
+                        <div className="i-title">
+                            <div className="i-title-wrapper">
+                                <div className="i-title-item">Java Developer</div>
+                                <div className="i-title-item">UI/UX</div>
+                                <div className="i-title-item">Python Developer</div>
+                                <div className="i-title-item">React Developer</div>
+                                <div className="i-title-item">Linux User</div>
+                            </div>
+                        </div>
+                        <div className="i-desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum ullam vero repellat quae fuga suscipit, beatae libero doloribus provident amet sunt, ipsum esse iure voluptatem quas nesciunt corrupti nisi dolore?</div>
+                    </div>
+                </div>
+                <div className="i-right">
+                    <img src={me} alt="" className="i-img"/>
+                </div>
+            </div>
+        </div>
+    )}
+export default Intro    
